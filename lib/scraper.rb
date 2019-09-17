1b15b8c44fb4919c27ddf8d4b4ca891f7616e105
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-09-17 17:53:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-
-Nokogiri::HTML(html)
-doc = Nokogiri::HTML(html)
-
-puts doc
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
